4f5d13e2351acf4e7ecec5d3632877b393d391da
Tomasz Rybarczyk <paluho@gmail.com>
2016-05-04 23:57:09 +0200

    Use only last part of constructor name (without module path)

--- src/Main.purs
+++ src/Main.purs
@@ -4,9 +4,11 @@ import Control.Alt (alt)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (log, CONSOLE)
 import Control.Plus (empty, class Plus)
+import Data.Array (last)
 import Data.Foldable (class Foldable, foldr)
 import Data.Generic (GenericSpine(SProd), fromSpine, GenericSignature(SigProd), toSignature, class Generic, gShow)
-import Data.Maybe (Maybe(Nothing))
+import Data.Maybe (fromMaybe, Maybe(Nothing))
+import Data.String (split)
 import Prelude (map, show, bind, Unit, class Show, (==), (<<<), ($))
 import Type.Proxy (Proxy(Proxy))
 
@@ -19,10 +21,13 @@ genericRead s p =
   spine = toSignature p
   step constructor =
     if constructorName == s
-      then fromSpine $ (SProd constructorName [])
+      then fromSpine $ (SProd fullConstructorName [])
       else Nothing
    where
-    constructorName = constructor.sigConstructor
+    -- leave only last part of constructor ie. Main.Foo -> Foo
+    fullConstructorName = constructor.sigConstructor
+    constructorName =
+      fromMaybe fullConstructorName (last <<< split "." $ fullConstructorName)
 
 -- this helper will be available in purescript-foldable-traversable 1.0
 oneOf :: forall f g a. (Foldable f, Plus g) => f (g a) -> g a
@@ -37,6 +42,6 @@ instance showSomething :: Show Something where
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   let p = Proxy :: (Proxy Something)
-  log <<< show <<< genericRead "Main.Foo" $ p
-  log <<< show <<< genericRead "Main.Bar" $ p
-  log <<< show <<< genericRead "Main.Baz" $ p
+  log <<< show <<< genericRead "Foo" $ p
+  log <<< show <<< genericRead "Bar" $ p
+  log <<< show <<< genericRead "Baz" $ p
